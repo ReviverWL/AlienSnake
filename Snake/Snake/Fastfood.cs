96a599391206bee8f8c9553c5eb40880b720bdbf
ReviverWL <hardssd@mail.ru>
2017-10-02 00:59:53 +0300

    Планы на создание еды

---
+++ Snake/Snake/Fastfood.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Fastfood
+    {
+        private int _x;
+        private int _y;
+        public Fastfood()
+        {
+            int x = -1;
+            _x = x;
+            int y = _y;
+            Random random = new Random();
+            x = random.Next(1, 148);
+            y = random.Next(1, 48);
+            Point food = new Point(x, y, '@');
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -15,6 +15,8 @@ namespace Snake
             Console.SetBufferSize(150, 50);
             Snake snake = new Snake(20, 6, Direction.Right);
             snake.ShowFigure();
+            Line line = new Line(0,  '+', 0, 49);
+            line.ShowFigure();
             while (true)
             {
                 Thread.Sleep(500);
